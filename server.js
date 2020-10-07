c2a298cd0fafa63dfc464ab866db4eb46d5e7251
NarutaUzumaki <orion.niklaev@gmail.com>
2020-10-07 17:42:05 +0300

    test

--- server.js
+++ server.js
@@ -17,21 +17,13 @@ var Message = mongoose.model('Message',{
 
 var dbURL = 'mongodb://localhost:27017/chat';
 
-
 app.get('/messages', (req, res) => {
 	Message.find({}, (err, messages) =>{
 		res.send(messages);
 	});
 });
 
-// app.post('/messages', (req, res) => {
-// 	var message = new Message(req.body);
-// 	message.save((err) =>{
-// 		if (err)
-// 			sendStatus(500);
-// 		res.sendStatus(200);
-// 	});
-// });
+
 
 app.post('/messages', (req, res) => {
 	var message = new Message(req.body);
